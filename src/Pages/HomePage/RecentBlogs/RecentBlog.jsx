fa7349513d58f6be21ad579fc3cf2ee7ca0625de
anikshajol <anikshajol@gmail.com>
2023-11-07 17:55:23 +0600

    recent blogs fetching problem solved

--- src/Pages/HomePage/RecentBlogs/RecentBlog.jsx
+++ src/Pages/HomePage/RecentBlogs/RecentBlog.jsx
@@ -15,7 +15,7 @@ const RecentBlog = ({ recentBlog }) => {
 
   return (
     <div>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <div className="card card-compact h-96 bg-base-100 shadow-xl">
         <figure>
           <img src={image} alt={title} />
         </figure>
--- src/Pages/HomePage/RecentBlogs/RecentBlogs.jsx
+++ src/Pages/HomePage/RecentBlogs/RecentBlogs.jsx
@@ -1,32 +1,47 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 import "react-loading-skeleton/dist/skeleton.css";
 import LoadingSkeleton from "../../../Spinner/LoadingSkeleton";
 import RecentBlog from "./RecentBlog";
 import { motion } from "framer-motion";
-import useAxios from "../../../hooks/useAxios";
+// import useAxios from "../../../hooks/useAxios";
+import useAuth from "../../../hooks/useAuth";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
 
 // import Skeleton from "react-loading-skeleton";
 
 const RecentBlogs = () => {
-  const axiosSecure = useAxios();
-  //   const [recentBlogs, setRecentBlogs] = useState([]);
-  //   const { loading } = useContext(AuthContext);
+  // const axiosSecure = useAxios();
+  const [recentBlogs, setRecentBlogs] = useState([]);
+  const { loading } = useAuth();
 
-  //   useEffect(() => {
-  //     axios.get("data.json").then((res) => {
-  //       console.log(res.data);
+  useEffect(() => {
+    axios.get("http://localhost:5000/blogs").then((res) => {
+      setRecentBlogs(res.data);
+    });
+  }, []);
+
+  // const { isLoading, data: recentBlogs } = useQuery({
+  //   queryKey: ["recentBlogs"],
+  //   queryFn: () => {
+  //     axios.get("http://localhost:5000/blogs").then((res) => {
+  //       return res.data;
   //     });
-  //   }, []);
+  //   },
+  // });
 
-  const { isLoading, data: recentBlogs } = useQuery({
-    queryKey: ["recentBlogs"],
-    queryFn: () => {
-      axiosSecure.get("/blogs").then((res) => {
-        return res.data;
-      });
-    },
-  });
-  if (isLoading) {
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/blogs")
+  //     .then((res) => res.json())
+  //     .then((data) => setRecentBlogs(data));
+  // }, []);
+
+  // console.log(recentBlogs);
+
+  if (loading) {
     const skeleton = Array.from({ length: 6 });
     return (
       <motion.div
@@ -45,7 +60,7 @@ const RecentBlogs = () => {
   console.log(recentBlogs);
 
   return (
-    <div className=" max-w-7xl mx-auto gap-4 grid grid-cols-3">
+    <div className=" max-w-7xl mx-auto gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {recentBlogs?.map((recentBlog, idx) => (
         <RecentBlog key={idx} recentBlog={recentBlog}></RecentBlog>
       ))}
