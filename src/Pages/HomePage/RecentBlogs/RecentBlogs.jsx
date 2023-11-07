2158ee3940b34a8b55c124e794900fa671ace7c9
anikshajol <anikshajol@gmail.com>
2023-11-07 15:08:23 +0600

    blog added by the server

--- src/Pages/HomePage/RecentBlogs/RecentBlogs.jsx
+++ src/Pages/HomePage/RecentBlogs/RecentBlogs.jsx
@@ -1,12 +1,14 @@
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import "react-loading-skeleton/dist/skeleton.css";
 import LoadingSkeleton from "../../../Spinner/LoadingSkeleton";
 import RecentBlog from "./RecentBlog";
 import { motion } from "framer-motion";
+import useAxios from "../../../hooks/useAxios";
+
 // import Skeleton from "react-loading-skeleton";
 
 const RecentBlogs = () => {
+  const axiosSecure = useAxios();
   //   const [recentBlogs, setRecentBlogs] = useState([]);
   //   const { loading } = useContext(AuthContext);
 
@@ -18,9 +20,10 @@ const RecentBlogs = () => {
 
   const { isLoading, data: recentBlogs } = useQuery({
     queryKey: ["recentBlogs"],
-    queryFn: async () => {
-      const res = await axios.get("/data.json");
-      return res.data;
+    queryFn: () => {
+      axiosSecure.get("/blogs").then((res) => {
+        return res.data;
+      });
     },
   });
   if (isLoading) {
@@ -43,7 +46,7 @@ const RecentBlogs = () => {
 
   return (
     <div className=" max-w-7xl mx-auto gap-4 grid grid-cols-3">
-      {recentBlogs.map((recentBlog, idx) => (
+      {recentBlogs?.map((recentBlog, idx) => (
         <RecentBlog key={idx} recentBlog={recentBlog}></RecentBlog>
       ))}
     </div>
