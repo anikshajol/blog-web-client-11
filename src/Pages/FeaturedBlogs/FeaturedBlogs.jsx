b027fa68d8b863fb31b0e5a758e6fb41089ba5f3
anikshajol <anikshajol@gmail.com>
2023-11-08 22:18:12 +0600

    featuredBlog added with react data table

--- src/Pages/FeaturedBlogs/FeaturedBlogs.jsx
+++ src/Pages/FeaturedBlogs/FeaturedBlogs.jsx
@@ -1,8 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import DataTable from "react-data-table-component";
+import Spinner from "../../Spinner/Spinner";
+
 const FeaturedBlogs = () => {
+  const { data: blogData, isLoading } = useQuery({
+    queryKey: ["featured"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/blogs");
+
+        console.log(response.data);
+        const sortedBlogs = response.data.sort(
+          (a, b) => a.long_description.length - b.long_description.length
+        );
+        const top10Blogs = sortedBlogs.slice(0, 10);
+        return top10Blogs;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+  });
+
+  if (isLoading) {
+    return <Spinner></Spinner>;
+  }
+
+  console.log(blogData);
+
+  const columns = [
+    {
+      name: "SL",
+      selector: (blogData, index) => index + 1,
+    },
+    {
+      name: "Title",
+      selector: (blogData) => blogData.title,
+    },
+    {
+      name: "Blog Owner",
+      selector: (blogData) => blogData.authorEmail,
+    },
+
+    {
+      name: "Photo",
+      selector: (blogData) => <img src={blogData.authorImage} alt="" />,
+    },
+  ];
+
   return (
     <div>
-      <h2>Featured Blogs</h2>
-      {/* <Table></Table> */}
+      <h2 className="text-2xl font-bold text-center">Featured Blogs</h2>
+      <div className="w-1/2 mx-auto">
+        <DataTable columns={columns} data={blogData} />
+      </div>
     </div>
   );
 };
