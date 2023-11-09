1dd83ffe47ef0341ca4d5b8b3b49fd5cff66c630
anikshajol <anikshajol@gmail.com>
2023-11-09 14:16:47 +0600

    jwt uses in login user and logout

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -11,6 +11,8 @@ import {
 import app from "../firebase/firebase.config";
 import PropTypes from "prop-types";
 
+import axios from "axios";
+
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
@@ -50,8 +52,28 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
       setUser(currentUser);
       setLoading(false);
+
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log("token response", res.data);
+          });
+      } else {
+        axios
+          .post("http://localhost:5000/logout", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      }
     });
 
     return () => unSubscribe();
